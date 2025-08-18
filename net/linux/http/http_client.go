c6a061b6f7c533b358d4b177b8dc8a525ea788a9
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-08-18 21:19:50 +0900

    ref: split stream tcp & cov up

--- net/linux/http/http_client.go
+++ net/linux/http/http_client.go
@@ -46,26 +46,9 @@ func (c *HttpClient) GET(host string, port uint16, path string) (core.HttpRespon
 		return core.HttpResponse{}, ErrFailedToCreateSocketAddress
 	}
 
-	conn, err := net.DialTCP("tcp", nil, addr)
+	received, err := StreamTCP(addr, host, path)
 	if err != nil {
-		return core.HttpResponse{}, ErrFailedToCreateSocketConnection
-	}
-	defer conn.Close()
-
-	// create request line to send -> connection
-	request := "GET " + path + " HTTP/1.1\n"
-	request += "Host: " + host + "\n"
-	request += "Accept: text/html\n"
-	request += "Connection: close\n\n"
-
-	if _, err := conn.Write([]byte(request)); err != nil {
-		return core.HttpResponse{}, ErrFailedToSendRequest
-	}
-
-	// read response
-	received := make([]byte, MaxResponseSize)
-	if _, err = conn.Read(received); err != nil {
-		return core.HttpResponse{}, ErrFailedToReceiveResponse
+		return core.HttpResponse{}, err
 	}
 
 	// create response
@@ -75,4 +58,38 @@ func (c *HttpClient) GET(host string, port uint16, path string) (core.HttpRespon
 	}
 
 	return res, nil
+}
+
+// stream tcp
+//  1. create tcp connection
+//  2. send request
+//  3. receive response
+func StreamTCP(addr *net.TCPAddr, host, path string) ([]byte, error) {
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		return []byte(""), ErrFailedToCreateSocketConnection
+	}
+	defer conn.Close()
+
+	request := createRequest(host, path)
+	if _, err := conn.Write([]byte(request)); err != nil {
+		return []byte(""), ErrFailedToSendRequest
+	}
+
+	// read response
+	received := make([]byte, MaxResponseSize)
+	if _, err = conn.Read(received); err != nil {
+		return []byte(""), ErrFailedToReceiveResponse
+	}
+
+	return received, nil
+}
+
+// create request line to send -> connection
+func createRequest(host, path string) string {
+	request := "GET " + path + " HTTP/1.1\n"
+	request += "Host: " + host + "\n"
+	request += "Accept: text/html\n"
+	request += "Connection: close\n\n"
+	return request
 }
--- net/linux/http/http_client_test.go
+++ net/linux/http/http_client_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/agiledragon/gomonkey/v2"
+	"github.com/poteto0/jagaimo/core"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -18,56 +19,103 @@ func TestHttpClient_GET(t *testing.T) {
 	// Arrange
 	client := NewHttpClient()
 
-	/*
-		t.Run("if failed to receive response, return ErrFailedToReceiveResponse", func(t *testing.T) {
-			patches := gomonkey.NewPatches()
-			defer patches.Reset()
+	t.Run("if success create response, return response", func(t *testing.T) {
+		patches := gomonkey.NewPatches()
+		defer patches.Reset()
 
-			// Mock
-			patches.ApplyFunc(
-				net.LookupIP,
-				func(host string) ([]net.IP, error) {
-					return []net.IP{net.IPv4(127, 0, 0, 1)}, nil
-				},
-			)
-			patches.ApplyFunc(
-				net.ResolveTCPAddr,
-				func(network, address string) (*net.TCPAddr, error) {
-					return &net.TCPAddr{
-						IP:   net.IPv4(127, 0, 0, 1),
-						Port: 80,
-						Zone: "",
-					}, nil
-				},
-			)
-			patches.ApplyFunc(
-				net.DialTCP,
-				func(network string, laddr, raddr *net.TCPAddr) (*net.TCPConn, error) {
-					return &net.TCPConn{}, nil
-				},
-			)
-			patches.ApplyMethod(
-				reflect.TypeOf(&net.TCPConn{}),
-				"Write",
-				func(_ *net.TCPConn, _ []byte) (int, error) {
-					return 10, nil
-				},
-			)
-			patches.ApplyMethod(
-				reflect.TypeOf(&net.TCPConn{}),
-				"Read",
-				func(_ *net.TCPConn, _ []byte) (int, error) {
-					return 0, net.UnknownNetworkError("test")
-				},
-			)
+		// Mock
+		patches.ApplyFunc(
+			net.LookupIP,
+			func(host string) ([]net.IP, error) {
+				return []net.IP{net.IPv4(127, 0, 0, 1)}, nil
+			},
+		)
+		patches.ApplyFunc(
+			net.ResolveTCPAddr,
+			func(network, address string) (*net.TCPAddr, error) {
+				return &net.TCPAddr{
+					IP:   net.IPv4(127, 0, 0, 1),
+					Port: 80,
+					Zone: "",
+				}, nil
+			},
+		)
+		patches.ApplyFunc(
+			StreamTCP,
+			func(addr *net.TCPAddr, host, path string) ([]byte, error) {
+				return []byte("hello"), nil
+			},
+		)
+		patches.ApplyFunc(
+			core.NewHttpResponse,
+			func(rawResponse string) (core.HttpResponse, error) {
+				return core.HttpResponse{
+					Version:    "HTTP/1.1",
+					StatusCode: 200,
+					Reason:     "OK",
+					Headers:    []core.Header{},
+					Body:       "hello",
+				}, nil
+			},
+		)
 
-			// Act
-			_, err := client.GET("test.example.com", 80, "/users")
+		// Act
+		res, err := client.GET("test.example.com", 80, "/users")
 
-			// Assert
-			assert.ErrorIs(t, ErrFailedToReceiveResponse, err)
-		})
-	*/
+		// Assert
+		assert.Nil(t, err)
+		assert.Equal(t, core.HttpResponse{
+			Version:    "HTTP/1.1",
+			StatusCode: 200,
+			Reason:     "OK",
+			Headers:    []core.Header{},
+			Body:       "hello",
+		}, res)
+	})
+
+	t.Run("if failed to create new response, return internal error", func(t *testing.T) {
+		patches := gomonkey.NewPatches()
+		defer patches.Reset()
+
+		// Arrange
+		expectedErr := net.UnknownNetworkError("test")
+
+		// Mock
+		patches.ApplyFunc(
+			net.LookupIP,
+			func(host string) ([]net.IP, error) {
+				return []net.IP{net.IPv4(127, 0, 0, 1)}, nil
+			},
+		)
+		patches.ApplyFunc(
+			net.ResolveTCPAddr,
+			func(network, address string) (*net.TCPAddr, error) {
+				return &net.TCPAddr{
+					IP:   net.IPv4(127, 0, 0, 1),
+					Port: 80,
+					Zone: "",
+				}, nil
+			},
+		)
+		patches.ApplyFunc(
+			StreamTCP,
+			func(addr *net.TCPAddr, host, path string) ([]byte, error) {
+				return []byte("hello"), nil
+			},
+		)
+		patches.ApplyFunc(
+			core.NewHttpResponse,
+			func(rawResponse string) (core.HttpResponse, error) {
+				return core.HttpResponse{}, expectedErr
+			},
+		)
+
+		// Act
+		_, err := client.GET("test.example.com", 80, "/users")
+
+		// Assert
+		assert.ErrorIs(t, expectedErr, err)
+	})
 
 	t.Run("if failed to send request, return ErrFailedToSendRequest", func(t *testing.T) {
 		patches := gomonkey.NewPatches()
