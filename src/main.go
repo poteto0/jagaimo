00187234c8d1fb0b03d1980d12feb43aa7ac95b5
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-09-14 20:09:45 +0900

    fin chap4

--- src/main.go
+++ src/main.go
@@ -3,20 +3,36 @@ package main
 import (
 	"fmt"
 
-	"github.com/poteto0/jagaimo/net/linux/http"
+	"github.com/poteto0/jagaimo/core"
+	"github.com/poteto0/jagaimo/core/browser"
 )
 
-func main() {
-	client := http.NewHttpClient()
+var testResponse = `HTTP/1.1 200 OK
+Data: xx xx xx
 
-	res, err := client.GET(
-		"localhost", 8000, "/test.html",
+
+<html>
+<head></head>
+<body>
+  <h1 id="title">H1 title</h1>
+  <h2 class="class">H2 title</h2>
+  <p>Test text.</p>
+  <p>
+    <a href="example.com">Link1</a>
+    <a href="example.com">Link2</a>
+  </p>
+</body>
+</html>
+`
+
+func main() {
+	saba := browser.NewBrowser()
+	page := saba.CurrentPage()
+
+	response, _ := core.NewHttpResponse(testResponse)
+	domString := page.ReceiveResponse(
+		response,
 	)
 
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-
-	fmt.Println(res)
+	fmt.Println(domString)
 }
